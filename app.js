e5c0085f136297a4c0c3601ae56d97f50a3f8d91
Daniele Salvigni <daniele.salvigni@nova.airbus.corp>
2018-05-15 16:34:49 +0200

    Weather API final release

---
+++ app.js
@@ -0,0 +1,41 @@
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+var URL = require('url');
+
+const arg = yargs
+    .options({
+        a:{
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch the weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+
+
+
+geocode.geocodeAddress(arg.address,(errorMessage, results) => {
+    if(errorMessage){
+        console.log(errorMessage);
+    } else {
+        //console.log(JSON.stringify(results,undefined,2));
+        console.log('Address: '+results.address);
+        //console.log('Latitude -> '+results.latitude);
+        //console.log('Longitude -> '+results.longitude);
+        weather.getWeather(results.latitude,results.longitude,(errorMessage, weatherResults) => {
+            if(errorMessage){
+                console.log(errorMessage);
+            } else {
+                console.log(JSON.stringify(weatherResults,undefined,2));
+            }
+        });
+    }
+});
+
+
